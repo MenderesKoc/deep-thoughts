0c8e064413414f30ab2fc13867469049e01c0258
Menderes Koc <mndrs.kc@gmail.com>
2022-05-17 20:04:35 -0400

    fixed package-json bugs

--- client/src/App.js
+++ client/src/App.js
@@ -40,6 +40,8 @@ function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/thought" element={<SingleThought />} />
+
+              <Route path="*" element={<NoMatch />} />
             </Routes>
           </div>
           <Footer />
