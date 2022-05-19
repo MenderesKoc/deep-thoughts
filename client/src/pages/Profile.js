5689517a909ead447bc54e0ed2a2c4864bcde974
Menderes Koc <mndrs.kc@gmail.com>
2022-05-19 10:08:03 -0400

    customize profile page for logged-in users

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
+import ThoughtList from "../components/ThoughtList";
+import FriendList from "../components/FriendList";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
 
   if (loading) {
     return <div>Loading...</div>;
