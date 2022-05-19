37843378c6a91ce4a9ef5e9b483a145f99c43983
Menderes Koc <mndrs.kc@gmail.com>
2022-05-19 10:57:45 -0400

    add thoughtFrom

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-import { useMutation } from "@apollo/client";
-import { LOGIN_USER } from "../utils/mutations";
-import Auth from "../utils/auth";
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
+
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -19,7 +21,7 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // execute addUser mutation and pass in variable data from form add token and set the local storage
+
     try {
       const { data } = await login({
         variables: { ...formState },
@@ -29,6 +31,12 @@ const Login = (props) => {
     } catch (e) {
       console.error(e);
     }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
   };
 
   return (
@@ -60,6 +68,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
